cf738cf4ac0d2702370caab4a66112ee5d0995eb
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-07-06 12:36:18 +0200

    Add files via upload

---
+++ 1-js-helloworld.js
@@ -0,0 +1,12 @@
+function btnFunction1() {
+    document.getElementById("text").innerHTML = "Hello World";
+}
+function btnFunction2() {
+    document.write("Button 2 was pressed...");
+}
+function btnFunction3() {
+    window.alert("Button 3 was pressed...");
+}
+function btnFunction4() {
+    console.log("Button 4 was pressed...");
+}
---
+++ 2-js-ssv.js
@@ -0,0 +1,20 @@
+//var x = 5.5, y = 7, z; // a;
+
+var $ = 5.5, y = 7, z;
+
+//x = 5.5;
+//y = 7;
+
+//z = x + y;
+z = $ + y;
+
+//This is a comment n1
+
+/*
+    This is a comment n2
+*/
+
+//document.getElementById("text").innerHTML = x + ' + ' + y + ' = ' + z;
+document.getElementById("text").innerHTML = $ + ' + ' + y + ' = ' + z;
+
+//document.getElementById("text").innerHTML = a;
