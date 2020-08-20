26e9890ee70aeab2c313dfc55b6007ec8e4820ee
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-08-20 07:46:01 +0200

    Add files via upload

---
+++ 3-operators.js
@@ -0,0 +1,24 @@
+//var x=3,y=4;
+//var z= x + y;
+
+//document.getElementById("text").innerHTML = x+" + "+y;
+//document.getElementById("result").innerHTML = z;
+
+//var x=3,y=4;
+//var z= x * y;
+
+//document.getElementById("text").innerHTML = x+" * "+y;
+//document.getElementById("result").innerHTML = z;
+
+//var x=3,y=4;
+
+//document.getElementById("text").innerHTML = x+" + "+y;
+
+//x += y;
+//var z= x;
+
+//document.getElementById("result").innerHTML = z;
+
+var txt= "This text does not make sense";
+txt += ", unless I add this.";
+document.getElementById("text").innerHTML = txt;
