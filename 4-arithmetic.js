75b0bcb8d77b4145401da41e2db4bfd41b36ad7d
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-08-28 09:36:10 +0200

    Add files via upload

---
+++ 4-arithmetic.js
@@ -0,0 +1,37 @@
+var x = 10;
+var y = 15;
+
+//var z = x + y;
+//document.getElementById("result").innerHTML = z;
+
+//var z = x - y;
+//document.getElementById("result").innerHTML = z;
+
+//var z = x * y;
+//document.getElementById("result").innerHTML = z;
+
+//var z = x / y;
+//document.getElementById("result").innerHTML = z;
+
+//var z = x % y;
+//document.getElementById("result").innerHTML = z;
+
+//x++;
+//document.getElementById("result").innerHTML = x;
+
+//x--;
+//document.getElementById("result").innerHTML = x;
+
+//x = 2;
+//y = x ** 3;
+//document.getElementById("result").innerHTML = y;
+
+//x = 2;
+//y = Math.pow(x,4);
+//document.getElementById("result").innerHTML = y;
+
+//x = 20 + 10 * 2;
+//document.getElementById("result").innerHTML = x;
+
+x = (20 + 10) * 2;
+document.getElementById("result").innerHTML = x;
