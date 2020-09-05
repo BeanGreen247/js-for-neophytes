75291b78b5eb1048fd5a3db37379b3bca734fad2
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-09-05 17:49:19 +0200

    Add files via upload

---
+++ 5-datatypes.js
@@ -0,0 +1,29 @@
+//var x = "items" + 12 + 8;
+
+//document.getElementById("result").innerHTML = x;
+
+//var x = 12;
+//x = "this is a text";
+
+//document.getElementById("result").innerHTML = x;
+
+//var x = "hello ";
+//var y = 'this is a "text"';
+
+//document.getElementById("result").innerHTML = x + y;
+
+//var x = 3;
+//var y = 3;
+//var z = 9;
+
+//var test = (x == z);
+
+//document.getElementById("result").innerHTML = test;
+
+//var test = ["apple","lemon","cherry"]
+
+//document.getElementById("result").innerHTML = test[0];
+
+var car = null;
+
+document.getElementById("result").innerHTML = typeof car;
