ab9ce27369a84b5947faf8b53f2cf8277c2ca51b
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-10-09 11:17:56 +0200

    Add files via upload

---
+++ 9-stringmethods.js
@@ -0,0 +1,57 @@
+//var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//var strln = txt.length;
+//console.log(strln);
+//document.getElementById('result').innerHTML = strln;
+
+//var sampletext = "Hi I want you to locate a word.";
+//var position = sampletext.indexOf("you");
+//document.getElementById('result').innerHTML = position;
+
+//var sampletext = "Hi I want you to locate a word called you.";
+//var position = sampletext.lastIndexOf("you");
+//document.getElementById('result').innerHTML = position;
+
+//var sampletext = "Hi I want you to locate a word called you.";
+//var position = sampletext.lastIndexOf("you",20);
+//document.getElementById('result').innerHTML = position;
+
+//var sampletext = "Hi I want you to locate a word.";
+//var position = sampletext.search("you");
+//document.getElementById('result').innerHTML = position;
+
+//var shoppinglist = "Apple, Banana, Kiwi";
+//var res = shoppinglist.slice(14);
+//document.getElementById('result').innerHTML = res;
+
+//var shoppinglist = "Apple, Banana, Kiwi";
+//var res = shoppinglist.substr(14);
+//document.getElementById('result').innerHTML = res;
+
+//str = "Please visit Microsoft and Microsoft!";
+//var n = str.replace("Microsoft", "BeanGreen247");
+//document.getElementById('result').innerHTML = n;
+
+//var text1 = "Hello World!";
+//var text2 = text1.toLowerCase();  
+//document.getElementById('result').innerHTML = text2;
+
+//var text1 = "Hello";
+//var text2 = "World";
+//var text3 = text1.concat(" ", text2);
+//document.getElementById('result').innerHTML = text3;
+
+//var textiscool= "         Hi this is for you John!        ";
+//var res = textiscool.trim();
+//document.getElementById('result').innerHTML = res;
+
+//var str = "hello world";
+//var res = str.charAt(4);
+//document.getElementById('result').innerHTML = res;
+
+//var str = "hello world";
+//var res = str.charCodeAt(0);
+//document.getElementById('result').innerHTML = res;
+
+var str = "hello world";
+var res = str[0];
+document.getElementById('result').innerHTML = res;
